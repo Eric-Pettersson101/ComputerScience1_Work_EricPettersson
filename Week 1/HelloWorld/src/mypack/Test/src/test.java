86e68b8390688a440831704a2ee022c7b48b5f31
Eric-Pettersson101 <64499355+Eric-Pettersson101@users.noreply.github.com>
2024-05-17 20:21:26 -0600

    Add files via upload

---
+++ Week 1/HelloWorld/src/mypack/Test/src/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+
+	public static void main(String[] args) {
+
+		final  int ARRAY_SIZE = 7;
+		int[] a = {1, 2, 4, 5};
+		System.out.println(a.length);
+	}
+
+}
---
+++ Week 2/JavaFundementals/src/mypack/HelloWorld.java
@@ -0,0 +1,15 @@
+package mypack;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+			final String MY_CON_STRING = "Constant";
+			String greeting = "Good morning";
+			String name = "Herman";
+			name = "Eric";
+			System.out.println(greeting + " "+ name);
+			System.out.println(MY_CON_STRING);
+
+	}
+
+}
